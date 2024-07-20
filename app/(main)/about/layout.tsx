d190c6a676a2a44c9b01b62a68a31168bcdd2e16
torytang <torytang.025@gmail.com>
2024-07-20 16:14:55 +0800

    feat: update about page metadata and Twitter card

--- app/(main)/about/layout.tsx
+++ app/(main)/about/layout.tsx
@@ -5,32 +5,34 @@ import AboutMe from "@/public/img/about-me.png";
 
 import AboutLoading from "./loading";
 
+const title = "I'm Tory Tang";
+const description =
+  "Highly motivated and experienced frontend  developer with a strong background in JavaScript, React, and performance optimization. Proven track record of delivering high-impact projects with significant improvements in user experience, conversion rates, and R&D efficiency. Passionate about open-source technologies, collaborative development, and continuous learning. Committed to leveraging cutting-edge frontend technologies to drive innovation and excellence.";
+
 export const metadata: Metadata = {
-  title: "I'm Tory Tang",
-  description: `Highly motivated and experienced frontend developer with a strong
-              background in JavaScript, React, and performance optimization.
-              Proven track record of delivering high-impact projects with
-              significant improvements in user experience, conversion rates, and
-              R&D efficiency. Passionate about open-source technologies,
-              collaborative development, and continuous learning. Committed to
-              leveraging cutting-edge frontend technologies to drive innovation
-              and excellence.`,
+  title,
+  description,
   openGraph: {
-    title: "I'm Tory Tang",
-    description: `Highly motivated and experienced frontend developer with a strong
-              background in JavaScript, React, and performance optimization.
-              Proven track record of delivering high-impact projects with
-              significant improvements in user experience, conversion rates, and
-              R&D efficiency. Passionate about open-source technologies,
-              collaborative development, and continuous learning. Committed to
-              leveraging cutting-edge frontend technologies to drive innovation
-              and excellence.`,
+    title,
+    description,
     images: [
       {
         url: AboutMe.src,
       },
     ],
     type: "profile",
+  },
+  twitter: {
+    images: [
+      {
+        url: AboutMe.src,
+      },
+    ],
+    title,
+    description,
+    card: "summary_large_image",
+    site: "@torytang025",
+    creator: "@torytang025",
   },
 };
 
